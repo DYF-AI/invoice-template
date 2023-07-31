2690ecb3e7af1fa873191022cbcfd682ed33ede8
dongyongfei <dongyongfei@dongyongfeideMacBook-Air.local>
2023-07-31 15:53:43 +0800

    add generate_seal.py

---
+++ scripts/step2_format_trans.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+# https://pypi.org/project/html2image/
+
+from html2image import Html2Image
+hti = Html2Image()
+def html2jpg(html_file:str):
+    pass
+
+
+if __name__ == "__main__":
+
+    hti.screenshot(
+        html_file='../medical-invoice/index.html', css_file='../medical-invoice/index.css',
+        save_as='medical_invoice.jpg',
+        size=(900, 600)
+    )
